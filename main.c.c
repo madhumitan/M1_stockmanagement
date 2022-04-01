8476487bc10b0ac355938faf553290b975a23c56
MADHU MITA N <48857664+madhumitan@users.noreply.github.com>
2022-04-01 17:24:44 +0530

    Delete main.c.c
    delete main file

--- main.c.c
+++
@@ -1,1 +0,0 @@
-#include<stdio.h>#include<string.h>#include "Storeitem.h"//#include<conio.h>char filename[] ="./storeitem.dat"; //binary filechar filename1[] ="./tempitem.dat";int main(){	Menu();	WriteRecord();	ListRecords();	delete();	return 0;}
