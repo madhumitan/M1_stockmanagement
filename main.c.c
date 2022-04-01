e322d18c6924e90095f710b77c449dfdfdfd01c8
MADHU MITA N <48857664+madhumitan@users.noreply.github.com>
2022-04-01 17:21:52 +0530

    Add files via upload
    main file

---
+++ main.c.c
@@ -0,0 +1,1 @@
+#include<stdio.h>#include<string.h>#include "Storeitem.h"//#include<conio.h>char filename[] ="./storeitem.dat"; //binary filechar filename1[] ="./tempitem.dat";int main(){	Menu();	WriteRecord();	ListRecords();	delete();	return 0;}
